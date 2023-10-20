00a0c7106866d8d37350e2a4eba61c1c45b74d74
ganeshtekale7800 <147368995+ganeshtekale7800@users.noreply.github.com>
2023-10-20 22:36:49 +0530

    Update good  first issue.php
    ok

--- 01/good  first issue.php
+++ 01/good  first issue.php
@@ -5,24 +5,24 @@ include('db.php');
 <html lang="en">
 <head>
 <title>PUNERI HOTEL</title>
-<!-- for-mobile-apps -->
+<!-- for-mobile-apps --> fgjfhgdgfgfh5ffhjf5f
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
 <meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-		function hideURLbar(){ window.scrollTo(0,1); } </script>
+		function hideURLbar(){ window.scrollTo(0,1); } </script>dfdjgfjgfjgjfgfjfjffg
 <!-- //for-mobile-apps -->
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" /> 
-<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="css/font-awesome.css" rel="stylesheet"> dfgdhghdhghdddgg2dg55g8g8dgd
 <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
 <link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
 <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
-<link rel="stylesheet" href="css/jquery-ui.css" />
+<link rel="stylesheet" href="css/jquery-ui.css" />khfkhgdkkjdjjdjgghdfg
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <script type="text/javascript" src="js/modernizr-2.6.2.min.js">
 <!--fonts-->
 <link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet"> 
-<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">gdfjgjdgdjgjjdd
 <link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"/>
 <!--//fonts-->
 </head>
